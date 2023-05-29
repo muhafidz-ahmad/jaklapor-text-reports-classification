69f78a763999b36354a5d78d92271427d3c833cf
Muhafidz Ahmad Halim <115754250+muhafidz-ahmad@users.noreply.github.com>
2023-05-29 20:25:11 +0700

    Create model.py

---
+++ model.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from keras.layers import *
+from keras.models import *
+from keras import backend as K
+
+import pickle
+
+import text_preprocessing
+
+class Att(Layer):
+    def __init__(self, **kwargs):
+        super(Att,self).__init__(**kwargs)
+ 
+    def build(self,input_shape):
+        self.W=self.add_weight(name='attention_weight', shape=(input_shape[-1],1), 
+                               initializer='random_normal', trainable=True)
+        self.b=self.add_weight(name='attention_bias', shape=(input_shape[1],1), 
+                               initializer='zeros', trainable=True)        
+        super(Att, self).build(input_shape)
+ 
+    def call(self,x):
+        # Alignment scores. Pass them through tanh function
+        e = K.tanh(K.dot(x,self.W)+self.b)
+        # Remove dimension of size 1
+        e = K.squeeze(e, axis=-1)   
+        # Compute the weights
+        alpha = K.softmax(e)
+        # Reshape to tensorFlow format
+        alpha = K.expand_dims(alpha, axis=-1)
+        # Compute the context vector
+        context = x * alpha
+        return K.sum(context, axis=1)
+      
+def load_model():
+    with st.spinner("Loading Model...."):
+        dependencies = {'Att': Att()}
+        model = tf.keras.models.load_model(
+          'models/best_model.h5',
+          custom_objects=dependencies
+        )
+        with open('models/tokenizer.pickle', 'rb') as handle:
+            tokenizer = pickle.load(handle)
+    return [model, tokenizer]
+  
+def predict(model, text):
+    # model[0] = model machine learning
+    # model[1] = tokenizer
+    preprocess_text = text_preprocessing.preprocess(text, stem=True)  # text_preprocessing
+    text = model[1].texts_to_sequences([preprocess_text])   # tokenizer
+    text = pad_sequences(text, padding='post',
+                         maxlen=100, truncating='post')
+    prediction = model[0].predict(text, verbose=0)
+    classes = np.argmax(prediction, axis = 1)
+    pred_class = dict_classes[classes[0]]
+    
+    df = pd.Series(prediction[0].round(decimals=5) * 100, 
+                   index=dict_classes.values()).sort_values(ascending=False)
+    df = df.to_frame().reset_index()
+    df = df.rename(columns={0: 'probability',
+                            'index': 'prediksi_kategori_laporan'})
+    
+    return df, preprocess_text
