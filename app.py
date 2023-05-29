249e2cc038b1af69bba4c73c0746ea215adcd252
Muhafidz Ahmad Halim <115754250+muhafidz-ahmad@users.noreply.github.com>
2023-05-29 20:45:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import model
 st.title("Public Report Classification")
 
 # load model
-my_model = model.load_model()
+with st.spinner("Loading Model...."):
+    my_model = model.load_model()
 
 # get new report
 new_report = st.text_area("Ada masalah apa?","")
@@ -15,7 +16,7 @@ new_report = st.text_area("Ada masalah apa?","")
 if st.button("Prediksi Kategori Laporan", use_container_width=True):
     with st.spinner("Tunggu sebentar, sedang memprediksi kategori laporan..."):
         df, preprocess_text = model.predict(my_model, new_report)
-        if st.checkbox('Tampilkan teks hasil preprocessing'):
+        with st.checkbox('Tampilkan teks hasil preprocessing'):
             st.write(preprocess_text)
         for cat, prob in zip(df['prediksi_kategori_laporan'], df['probability']):
             if prob < 5:
@@ -23,5 +24,5 @@ if st.button("Prediksi Kategori Laporan", use_container_width=True):
             pred = cat + " | " + str(round(prob,2)) + "%"
             st.success(pred)
         st.divider() # draw a horizontal line
-        if st.checkbox('Tampilkan semua kategori'):
+        with st.checkbox('Tampilkan semua kategori'):
             st.dataframe(df, use_container_width=True)
