655797e10419dc77bf1fe40de506a8f9b5110dbb
Muhafidz Ahmad Halim <115754250+muhafidz-ahmad@users.noreply.github.com>
2023-05-29 20:47:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,13 +16,18 @@ new_report = st.text_area("Ada masalah apa?","")
 if st.button("Prediksi Kategori Laporan", use_container_width=True):
     with st.spinner("Tunggu sebentar, sedang memprediksi kategori laporan..."):
         df, preprocess_text = model.predict(my_model, new_report)
-        with st.checkbox('Tampilkan teks hasil preprocessing'):
-            st.write(preprocess_text)
         for cat, prob in zip(df['prediksi_kategori_laporan'], df['probability']):
             if prob < 5:
                 break
             pred = cat + " | " + str(round(prob,2)) + "%"
             st.success(pred)
-        st.divider() # draw a horizontal line
-        with st.checkbox('Tampilkan semua kategori'):
-            st.dataframe(df, use_container_width=True)
+    
+    st.divider() # draw a horizontal line
+    
+    # show preprocessed text
+    if st.checkbox('Tampilkan teks hasil preprocessing'):
+        st.write(preprocess_text)
+        
+    # show table of all categories
+    if st.checkbox('Tampilkan semua kategori'):
+        st.dataframe(df, use_container_width=True)
