4df92e1c493afdc6124e4f4760286302ee1150b6
Muhafidz Ahmad Halim <115754250+muhafidz-ahmad@users.noreply.github.com>
2023-06-02 08:52:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ new_report = st.text_area("Ada masalah apa?","")
 
 # Define the CSS style for the cards
 card_style = """
+    border-style: solid;
+    border-width: thick;
     border-color: #F9A0C6;
     border-radius: 10px;
     box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.15);
